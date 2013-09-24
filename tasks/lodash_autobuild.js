1fbaec53dfa5ab31add6c67e121e4274624c0bc4
jjt <jason@jasontrill.com>
2013-09-23 18:59:38 -0700

    Initial commit

---
+++ tasks/lodash_autobuild.js
@@ -0,0 +1,40 @@
+/*
+ * grunt-lodash-autobuild
+ * https://github.com/jjt/grunt-lodash-autobuild
+ *
+ * Copyright (c) 2013 jjt
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('lodashAutobuild', 'Analyzes your source js/cs/html/etc. and custom builds a minimal version of lodash', function() {
+    var options = this.options({
+      lodashConfigPath: 'lodash.build.options.include'
+    });
+
+    var shelljs = require('shelljs');
+    var _ = require('lodash');
+    var fns = [], fnsStr = '';
+
+    this.data.forEach(function(f) {
+      var files = grunt.file.expand(f);
+      var matchlines = shelljs.grep('_\.\w*', files);
+      var matches = matchlines.match(/_\.\w*/gi);
+      if(!_.isArray(matches))
+        return;
+      matches.forEach(function(match, i) {
+        matches[i] = match.replace('_.', '');
+      });
+      fns = fns.concat(matches);
+    });
+
+    fns = _.uniq(fns).sort();
+    fnsStr = fns.join(', ')
+    grunt.config.set(options.lodashConfigPath, fnsStr);
+    grunt.log.write("Found the following lodash functions:\n" + fnsStr);
+    grunt.task.run('lodash')
+  });
+
+};
