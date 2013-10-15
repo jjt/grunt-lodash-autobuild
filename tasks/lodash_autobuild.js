8620a4a2b33176a649e2573a1f0c5f584c81fd69
jjt <jason@jasontrill.com>
2013-10-14 20:59:25 -0700

    feature: Add config option for lodash Object variable name
    Closes issue #1

--- tasks/lodash_autobuild.js
+++ tasks/lodash_autobuild.js
@@ -11,33 +11,38 @@
 module.exports = function(grunt) {
   grunt.registerMultiTask('lodashAutobuild', 'Analyzes your source js/cs/html/etc. and custom builds a minimal version of lodash', function() {
     var options = this.options({
-      lodashConfigPath: 'lodash.build.options.include'
+      lodashConfigPath: 'lodash.build.options.include',
+      lodashObjects: [ '_' ]
     });
 
-    var shelljs = require('shelljs');
-    var _ = require('lodash');
-    var fns = [], fnsStr = '';
-    var filesIn = this.data
-
+    var shelljs = require('shelljs'),
+      _ = require('lodash'),
+      fns = [],
+      fnsStr = '',
+      filesIn = this.data,
+      lodashObjectsStr = '(' + options.lodashObjects.join('|') + ')',
+      lodashObjectsPattern = lodashObjectsStr + '\\.',
+      lodashUsagePattern = lodashObjectsPattern + '\\w*';
+   
     if(_.isString(filesIn))
       filesIn = [filesIn] 
 
     filesIn.forEach(function(f) {
       var files = grunt.file.expand(f);
-      var matchlines = shelljs.grep('_\.\w*', files);
-      var matches = matchlines.match(/_\.\w*/gi);
+      var matchlines = shelljs.grep(lodashUsagePattern, files);
+      var matches = matchlines.match(new RegExp(lodashUsagePattern, 'gi'));
       if(!_.isArray(matches))
         return;
       matches.forEach(function(match, i) {
-        matches[i] = match.replace('_.', '');
+        matches[i] = match.split('.').slice(-1);
       });
       fns = fns.concat(matches);
     });
 
     fns = _.uniq(fns).sort();
     fnsStr = fns.join(', ')
+    grunt.log.write("Found the following lodash functions:\n" + fnsStr + "\n");
     grunt.config.set(options.lodashConfigPath, fnsStr);
-    grunt.log.write("Found the following lodash functions:\n" + fnsStr);
     grunt.task.run('lodash')
   });
 
